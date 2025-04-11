3ed49ce9094da8e31f2985cc796311567cca6554
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-04-11 15:42:51 +0100

    Remove test_cli.py

--- tests/test_cli.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-import sys
-
-from dan_fernandes_test_python_copier_template import __version__
-
-
-def test_cli_version():
-    cmd = [sys.executable, "-m", "dan_fernandes_test_python_copier_template", "--version"]
-    assert subprocess.check_output(cmd).decode().strip() == __version__

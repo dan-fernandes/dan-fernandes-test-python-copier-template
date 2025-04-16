b509db3f57404d1e6b7b2a9da28a6109249289c5
Daniel Fernandes <danieljosephfernandes@gmail.com>
2025-04-16 11:37:17 +0100

    Add double

--- src/dan_fernandes_test_python_copier_template/main.py
+++ src/dan_fernandes_test_python_copier_template/main.py
@@ -1,2 +1,6 @@
 def f():
     return 0
+
+
+def double(x: int) -> int:
+    return 2 * x
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,9 @@
-from dan_fernandes_test_python_copier_template.main import f
+from dan_fernandes_test_python_copier_template.main import double, f
 
 
 def test_f():
     assert f() == 0
+
+
+def test_double():
+    assert double(2) == 4
